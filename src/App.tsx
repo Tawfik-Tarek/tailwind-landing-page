9cac58fbe0e6e2edd29f1de3498311ec736bd2b2
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-09-18 07:40:10 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-red-600 min-h-[100dvh]'>
+      <h1 className='text-white text-3xl'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
