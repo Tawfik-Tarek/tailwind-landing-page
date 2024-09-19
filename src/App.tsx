9661c7826748d8efeab862a7e11675514bf6df67
Tawfik-Tarek <tawfiktarek1975@gmail.com>
2024-09-20 01:56:11 +0300

    finish animations

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
     <div className="py-16 px-4 min-h-dvh grid place-items-center">
       <div className="grid grid-cols-1 xl:grid-cols-[auto_1fr] gap-12 sm:gap-16 xl:gap-24 place-items-center ">
         <div className="text-center flex flex-col items-center max-w-md xl:items-start xl:text-left xl:order-2">
-          <EpicStackLogo className="size-20  mb-6 md:mb-8" />
-          <h1 className="text-4xl  mb-4 font-medium sm:text-4.5xl md:text-5xl lg:text-5.5xl md:mb-6">
+          <EpicStackLogo className="size-20  mb-6 md:mb-8 animate-slideUp xl:animate-slideLeft xl:[animation-delay:1.6s] xl:[animation-fill-mode:backwards]" />
+          <h1 className="text-4xl  mb-4 font-medium sm:text-4.5xl md:text-5xl lg:text-5.5xl md:mb-6 animate-slideUp [animation-delay:0.3s] [animation-fill-mode:backwards] xl:animate-slideLeft xl:[animation-delay:2s] xl:[animation-fill-mode:backwards]">
             The <span className="text-highlight">Epic</span> Stack
           </h1>
-          <p className="text-slate-600 sm:text-lg md:text-xl">
+          <p className="text-slate-600 sm:text-lg md:text-xl animate-slideUp [animation-delay:0.6s] [animation-fill-mode:backwards] xl:animate-slideLeft xl:[animation-delay:2.5s] xl:[animation-fill-mode:backwards]">
             Check the{" "}
             <a
               className="underline text-black hover:no-underline focus:ring-2 focus:ring-offset-2 focus:ring-highlight"
@@ -25,10 +25,11 @@ function App() {
         </div>
 
         <ul className="flex flex-wrap gap-2 justify-center max-w-3xl sm:gap-4 xl:grid xl:grid-cols-5 xl:grid-rows-6 xl:grid-flow-col">
-          {logos.map((Logo) => (
+          {logos.map((Logo , index) => (
             <li
+            style={{animationDelay: `${index * 0.08}s`}}
               key={Logo.src}
-              className={`col-start-${Logo.column} row-start-${Logo.row} `}
+              className={`col-start-${Logo.column} row-start-${Logo.row} animate-roal [animation-fill-mode:backwards]`}
             >
               <a
                 href={Logo.href}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  safelist:[
-    {pattern: /col-start-(1|2|3|4|5)/},
-    {pattern: /row-start-(1|2|3|4|5|6)/},
+  safelist: [
+    { pattern: /col-start-(1|2|3|4|5)/ },
+    { pattern: /row-start-(1|2|3|4|5|6)/ },
   ],
   theme: {
     extend: {
-      colors:{
-        "highlight": "#6202FF",
-      }, 
-      screens:{
-        "sm" : "520px",
-        "lg" : "976px",
+      colors: {
+        highlight: "#6202FF",
       },
-      fontFamily:{
-        popines : ["Poppins", "sans-serif"],
+      screens: {
+        sm: "520px",
+        lg: "976px",
       },
-      fontSize:{
-        "4.5xl" : ["2.625rem", "1.15"],
-        "5.5xl" :['3.375rem' , '1']
-      }
+      fontFamily: {
+        popines: ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        "4.5xl": ["2.625rem", "1.15"],
+        "5.5xl": ["3.375rem", "1"],
+      },
+      keyframes: {
+        slideUp: {
+          "0%": { transform: "translateY(120px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        slideLeft: {
+          "0%": { transform: "translateX(20px)", opacity: 0 },
+          "100%": { transform: "translateX(0)", opacity: 1 },
+        } ,
+        roal : {
+          "0%": { transform: "rotate(12deg)" , scale : 0 , opacity : 0},
+          "100%": { transform: "rotate(0deg)" , scale : 1 , opacity : 1},
+        }
+      },
+      animation: {
+        slideUp: "slideUp 0.5s ease-out",
+        slideLeft: "slideLeft 0.5s ease-out",
+        roal : "roal 0.11s cubic-bezier(.26,.74,.78,.75)"
+      },
     },
   },
   plugins: [],
